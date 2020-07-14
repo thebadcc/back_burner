2af3f0e5c1223ef93409e2e7037c4ee2d1b64c17
thebadcc <thebadcc@gmail.com>
2020-07-13 23:58:35 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,4 @@ var errorCorrectionLevel = 'L';
 var qr = qrcode(typeNumber, errorCorrectionLevel);
 qr.addData('www.google.com');
 qr.make();
-document.getElementById('qrcode').innerHTML = qr.createImgTag(2, 0);
+document.getElementById('qrcode').innerHTML = qr.createImgTag();
