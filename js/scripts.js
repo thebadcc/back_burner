67d57014c4947588fb2dd0d731fa9989ce6ea034
thebadcc <thebadcc@gmail.com>
2020-07-14 21:19:24 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,3 @@
-var typeNumber = 4;
-var errorCorrectionLevel = 'L';
-var qr = qrcode(typeNumber, errorCorrectionLevel);
-qr.addData('www.google.com');
-qr.make();
-document.getElementById('qrcode').innerHTML = qr.createImgTag();
-
-
 $(document).ready(function() {
   $('.open-menu').on('click', function() {
      $('.overlay').addClass('open');
