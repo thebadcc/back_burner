0225dbba6513969edd9f9009e4c4dc47312c6cb5
thebadcc <thebadcc@gmail.com>
2020-07-15 14:56:20 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -24,9 +24,15 @@ xhr.addEventListener("readystatechange", function () {
     var h = e.assets[f].name;
     var i = e.assets[f].traits;
     console.log(i);
-
+    if (i > 2) {
+    var j = e.assets[f].traits[2];
+    } else {
+    var j = e.assets[f].traits[1];  
+    }
+    console.log(j);
+    var k =  h + " by " + j;
     document.body.style.backgroundImage = "url(" + g + ")";
-    document.getElementById('attribution').innerHTML = i
+    document.getElementById('attribution').innerHTML = k
   }
 });
 xhr.open("GET", "https://api.opensea.io/api/v1/assets?owner=0x844289f123E2284a57Bc18264115548d91CC7597&asset_contract_address=0xfbeef911dc5821886e1dda71586d90ed28174b7d&asset_contract_addresses=%5B%5D&order_direction=desc&offset=0&limit=20");
