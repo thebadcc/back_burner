4ed8cf1446bde7e3cfb0e4b23243d08eba613969
thebadcc <thebadcc@gmail.com>
2020-07-15 12:39:02 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -18,13 +18,12 @@ d.style.fontFamily = b;
 var xhr = new XMLHttpRequest();
 xhr.addEventListener("readystatechange", function () {
   if (this.readyState === this.DONE) {
-    console.log(xhr.response.assets[0]);
+    console.log(xhr.response);
   }
 });
 xhr.open("GET", "https://api.opensea.io/api/v1/assets?owner=0x844289f123E2284a57Bc18264115548d91CC7597&asset_contract_address=0xfbeef911dc5821886e1dda71586d90ed28174b7d&asset_contract_addresses=%5B%5D&order_direction=desc&offset=0&limit=20");
 xhr.send();
 
 var g = "https://ipfs.infura.io/ipfs/Qmb9s7oM9Ym9Z2LmEfLNyUScacskQxcDyzwKKh9fzBgubk/asset.jpeg";
-
 document.body.style.backgroundImage = "url(" + g + ")";
 
