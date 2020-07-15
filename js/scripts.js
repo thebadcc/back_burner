6a96e0177afdaca74e3c80ef42dca2b9d14f3f76
thebadcc <thebadcc@gmail.com>
2020-07-14 23:49:30 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -11,5 +11,6 @@ $(document).ready(function() {
 var a = Math.floor(Math.random() * 2) + 1;
 var b = "F" + a;
 var c = document.getElementById("logo");
+var d = document.getElementById("attribution");
 c.style.fontFamily = b;
-  
+d.style.fontFamily = b;  
