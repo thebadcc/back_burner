de756c77f3194554cfdb0da880afbe82e2ab62e2
thebadcc <thebadcc@gmail.com>
2020-07-15 00:06:34 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,14 @@ var c = document.getElementById("logo");
 var d = document.getElementById("attribution");
 c.style.fontFamily = b;
 d.style.fontFamily = b;  
+
+var data = null;
+var xhr = new XMLHttpRequest();
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === this.DONE) {
+    console.log(this.responseText);
+  }
+});
+xhr.open("GET", "https://api.opensea.io/api/v1/assets?owner=0x844289f123E2284a57Bc18264115548d91CC7597&asset_contract_address=0xfbeef911dc5821886e1dda71586d90ed28174b7d&asset_contract_addresses=%5B%5D&order_direction=desc&offset=0&limit=20");
+xhr.send(data);
+
